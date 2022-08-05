5dea1773c62a224bed292725e7dc0f9c98febff9
Risang Daniswara <akurisang12@gmail.com>
2022-08-05 12:57:18 +0700

    fix listing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,7 @@
 @extends('products.layout')
 
+
+
 @section('content')
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -24,15 +26,33 @@
             @csrf
             <div class="col-lg-20 margin-tb">
                 <div class="pull-right" style="margin-top: 20px;">
-                <button type="submit" class="btn btn-danger">
-                    <i class="material-icons">logout</i>
-                    <span class="button-text">Keluar</span>
-                </button>
+                    <button type="submit" class="btn btn-danger">
+                        <i class="material-icons">logout</i>
+                        <span class="button-text">Keluar</span>
+                    </button>
                 </div>
             </div>
         </form>
+        <!-- <div class="pull-left">
+            
+            <a class="btn btn-danger">
+                <i class="material-icons" >logout</i> 
+                <span class="button-text" >Keluar</span>
+            </a> 
+
+                <a class="btn btn-success" href="{{ route('admin.create') }}"> Buat Pengaduan Baru</a>
+
+            </div> -->
     </div>
+
 </div>
+<!-- <div class="pull-left" style="margin-bottom: 20px;">
+
+    <a class="btn btn-success" href="{{ route('admin.create') }}"> Buat Pengaduan Baru</a>
+
+</div> -->
+
+
 
 @if ($message = Session::get('success'))
 
@@ -48,13 +68,38 @@
 
 <table class="table table-bordered" style="margin-top: 20px;">
 
+    <tr>
+
+        <th>No</th>
+
+        <th>Nama</th>
+
+        <th>Subjek</th>
+
+        <th width="280px">Action</th>
+
+    </tr>
+
+    @foreach ($products as $product)
+
+    <tr>
+
+        <td>{{ ++$i }}</td>
+
+        <td>{{ $product->nama }}</td>
+
+        <td>{{ $product->subjek  }}</td>
+
+        <td>
 
             <form action="{{ route('products.destroy',$product->id) }}" method="POST">
 
                 <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">
-                    <i class="material-icons" >visibility</i>
-                    <span class="button-text" >Detail</span>
+                    <i class="material-icons">visibility</i>
+                    <span class="button-text">Detail</span>
                 </a>
+
+                <!-- <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a> -->
 
                 @csrf
 
