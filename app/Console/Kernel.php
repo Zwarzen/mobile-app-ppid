5c11ce7aeb76b9ceebed19c7c1fa6ca069e7ee45
Risang Daniswara <akurisang12@gmail.com>
2022-08-09 08:13:25 +0700

    trial backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,32 +1,81 @@
 <?php
 
+  
+
 namespace App\Console;
 
+  
+
 use Illuminate\Console\Scheduling\Schedule;
+
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+  
+
 class Kernel extends ConsoleKernel
+
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
 
     /**
-     * Register the commands for the application.
+
+     * The Artisan commands provided by your application.
+
      *
-     * @return void
+
+     * @var array
+
      */
-    protected function commands()
+
+    protected $commands = [
+
+        'App\Console\Commands\DatabaseBackUp'
+
+    ];
+
+  
+
+    /**
+
+     * Define the application's command schedule.
+
+     *
+
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+
+     * @return void
+
+     */
+
+    protected function schedule(Schedule $schedule)
+
     {
+
+        $schedule->command('database:backup')->daily();
+
+    }
+
+  
+
+    /**
+
+     * Register the commands for the application.
+
+     *
+
+     * @return void
+
+     */
+
+    protected function commands()
+
+    {
+
         $this->load(__DIR__.'/Commands');
 
+  
+
         require base_path('routes/console.php');
+
     }
+
 }
